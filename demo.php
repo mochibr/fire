db80a5f6de71761aee549f6baafc3282d5e8de49
mochibr <105723049+mochibr@users.noreply.github.com>
2022-11-28 18:43:45 +0530

    demo

---
+++ demo.php
@@ -0,0 +1,29 @@
+What is React?
+React is a JavaScript library created by Facebook
+
+What is React JS?
+React JS is an open-source JavaScript library for developed by Facebook. It’s used for building interactive user interfaces based on UI components and single-page applications.
+
+React JS is an open-source JavaScript library for building user interfaces – usually for single-page and mobile applications.
+
+why use reactjs ?
+1. Easy to learn
+simple to use
+2. Flexibility
+3. Reusable components
+4. Fast Rendering
+5. SEO Friendly
+2. Strong community support
+6. Open source
+
+
+1. HTML and CSS
+2. VANILLA JAVASCRIPT
+    let, const and Array
+    Loops (for, forEach, for-of, map, Filter, Reduce)
+    Arrow functions
+    ES6 classes
+    Destructuring assignment
+    Array functions especially Map and filter
+    ES6 modules
+    Asynchronous JavaScript
