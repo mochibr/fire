9b1da2c558ae885b9540fe31f578b656a7123315
mochibr <105723049+mochibr@users.noreply.github.com>
2023-09-03 13:44:32 +0530

    Update demo.php

--- demo.php
+++ demo.php
@@ -1,29 +1,171 @@
-     <div class="container">
-        <div class="custom-select open">
-            <div class="select-box">
-                <input type="text" class="tags_input" name="tags" hidden="">
-                <div class="selected-options">
-                    <span class="tag">Black<span class="remove-tag" data-value="Black">×</span></span><span class="tag">Green<span class="remove-tag" data-value="Green">×</span></span><span class="tag">Navy<span class="remove-tag" data-value="Navy">×</span></span><span class="tag">Orange<span class="remove-tag" data-value="Orange">×</span></span><span class="tag">+5</span></div>
-                <div class="arrow"><i class="fa fa-angle-down" aria-hidden="true"></i></div>
-            </div>
-            <div class="options">
-                <div class="option-search-tags">
-                    <input type="text" class="search-tags" placeholder="Search tags...">
-                    <button type="button" class="clear"><i class="fa fa-close"></i></button>
-                </div>
-                <div class="option all-tags" data-value="All">All</div>
-                <div class="option active" data-value="Black">Black</div>
-                <div class="option active" data-value="Green">Green</div>
-                <div class="option" data-value="Navy">Navy</div>
-                <div class="option" data-value="Orange">Orange</div>
-                <div class="option" data-value="Pink">Pink</div>
-                <div class="option" data-value="Red">Red</div>
-                <div class="option" data-value="Sliver">Sliver</div>
-                <div class="option" data-value="White">White</div>
-                <div class="option" data-value="Yellow">Yellow</div>
-                <div class="no-result-message">No result match</div>
-            </div>
-            <spna class="tag_error_msg error">This field is required</spna>
-        </div>
-        <input type="button" class="btn_submit" value="Submit" /> 
-    </div>
+document.addEventListener("DOMContentLoaded", function(){
+    const customSelects = document.querySelectorAll(".custom-select");
+
+    function updateSelectedOptions(customSelect){
+        const selectedOptions = Array.from(customSelect.querySelectorAll(".option.active")).filter(option => option !== customSelect.querySelector(".option.all-tags")).map(function(option){
+            return {
+                value: option.getAttribute("data-value"),
+                text: option.textContent.trim()
+            };
+        });
+
+        const selectedValues = selectedOptions.map(function(option){
+            return option.value;
+        });
+
+        customSelect.querySelector(".tags_input").value = selectedValues.join(', ');
+
+        let tagsHTML = "";
+
+        if(selectedOptions.length === 0){
+            tagsHTML = '<span class="placeholder">Select the tags</span>';
+        }else{
+            const maxTagsToShow = 4;
+            let additionalTagsCount = 0;
+
+            selectedOptions.forEach(function(option, index){
+                if(index < maxTagsToShow){
+                    tagsHTML += '<span class="tag">'+option.text+'<span class="remove-tag" data-value="'+option.value+'">&times;</span></span>';
+                }else{
+                    additionalTagsCount++;
+                }
+            });
+
+            if(additionalTagsCount > 0){
+                tagsHTML += '<span class="tag">+'+additionalTagsCount+'</span>';
+            }
+        }
+        customSelect.querySelector(".selected-options").innerHTML = tagsHTML;
+    }
+
+    customSelects.forEach(function(customSelect){
+        const searchInput = customSelect.querySelector(".search-tags");
+        const optionsContainer = customSelect.querySelector(".options");
+        const noResultMessage = customSelect.querySelector(".no-result-message");
+        const options = customSelect.querySelectorAll(".option");
+        const allTagsOption = customSelect.querySelector(".option.all-tags");
+        const clearButton = customSelect.querySelector(".clear");
+
+        allTagsOption.addEventListener("click", function(){
+            const isActive = allTagsOption.classList.contains("active");
+
+            options.forEach(function(option){
+                if(option !== allTagsOption){
+                    option.classList.toggle("active", !isActive);
+                }
+            });
+
+            updateSelectedOptions(customSelect);
+        });
+
+        clearButton.addEventListener("click", function(){
+            searchInput.value = "";
+            options.forEach(function(option){
+                option.style.display = "block";
+            });
+            noResultMessage.style.display = "none";
+        });
+
+        searchInput.addEventListener("input", function(){
+            const searchTerm = searchInput.value.toLowerCase();
+
+            options.forEach(function(option){
+                const optionText = option.textContent.trim().toLocaleLowerCase();
+                const shouldShow = optionText.includes(searchTerm);
+                option.style.display = shouldShow ? "block" : "none";
+            });
+
+            const anyOptionsMatch = Array.from(options).some(option => option.style.display === "block");
+            noResultMessage.style.display = anyOptionsMatch ? "none" : "block";
+
+            if(searchTerm){
+                optionsContainer.classList.add("option-search-active");
+            }else{
+                optionsContainer.classList.remove("option-search-active");
+            }
+        });
+    });
+
+    customSelects.forEach(function(customSelect){
+        const options = customSelect.querySelectorAll(".option");
+        options.forEach(function(option){
+            option.addEventListener("click", function(){
+                option.classList.toggle("active");
+                updateSelectedOptions(customSelect);
+            });
+        });
+    });
+
+    document.addEventListener("click", function(event){
+        const removeTag = event.target.closest(".remove-tag");
+        if(removeTag){
+            const customSelect = removeTag.closest(".custom-select");
+            const valueToRemove = removeTag.getAttribute("data-value");
+            const optionToRemove = customSelect.querySelector(".option[data-value='"+valueToRemove+"']");
+            optionToRemove.classList.remove("active");
+
+            const otherSelectedOptions = customSelect.querySelectorAll(".option.active:not(.all-tags)");
+            const allTagsOption = customSelect.querySelector(".option.all-tags");
+
+            if(otherSelectedOptions.length === 0){
+                allTagsOption.classList.remove("active");
+            }
+            updateSelectedOptions(customSelect);
+        }
+    });
+
+    const selectBoxes = document.querySelectorAll(".select-box");
+    selectBoxes.forEach(function(selectBox){
+        selectBox.addEventListener("click",function(event){
+            if(!event.target.closest(".tag")){
+                selectBox.parentNode.classList.toggle("open");
+            }
+        });
+    });
+
+    document.addEventListener("click",function(event){
+        if(!event.target.closest(".custom-select") && !event.target.classList.contains("remove-tag")){
+            customSelects.forEach(function(customSelect){
+                customSelect.classList.remove("open");
+            });
+        }
+    });
+
+    function resetCustomSelects(){
+        customSelects.forEach(function(customSelect){
+            customSelect.querySelectorAll(".option.active").forEach(function(option){
+                option.classList.remove("active");
+            });
+            customSelect.querySelector(".option.all-tags").classList.remove("active");
+            updateSelectedOptions(customSelect);
+        });
+    }
+    updateSelectedOptions(customSelects[0]);
+
+    const submitButton = document.querySelector(".btn_submit");
+    submitButton.addEventListener("click", function(){
+        let valid = true;
+
+        customSelects.forEach(function(customSelect){
+            const selectedOptions = customSelect.querySelectorAll(".option.active");
+
+            if(selectedOptions.length === 0){
+                const tagErrorMsg = customSelect.querySelector(".tag_error_msg");
+                tagErrorMsg.textContent = "This field is required";
+                tagErrorMsg.style.display = "block";
+                valid = false;
+            }else{
+                const tagErrorMsg = customSelect.querySelector(".tag_error_msg");
+                tagErrorMsg.textContent = "";
+                tagErrorMsg.style.display = "none";
+            }
+        });
+
+        if(valid){
+            let tags = document.querySelector(".tags_input").value;
+            alert(tags);
+            resetCustomSelects();
+            return;
+        }
+    });
+});
